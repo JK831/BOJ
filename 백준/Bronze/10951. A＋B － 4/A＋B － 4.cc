80d4844ad1d79471d3ac3f9d899e4338d7820c30
ManInJoongTham <75526576+JK831@users.noreply.github.com>
2022-03-05 20:43:40 +0900

    [Bronze III] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int a, b;
+
+void Solution()
+{
+    while(scanf("%d %d", &a, &b) != -1)
+		printf("%d\n", a + b);
+}
+
+void Solve()
+{
+    Solution();
+}
+
+int main(int argc, const char * argv[]) {
+ 
+	Solve();	
+ 
+	return 0;
+}
